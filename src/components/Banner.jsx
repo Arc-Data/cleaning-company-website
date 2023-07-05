e5592b9cc422f2ed140328558ef8c8fc0359aa26
Arc-Data <arclight04@gmail.com>
2023-07-05 13:25:13 +0800

    creates about section in home page

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,6 @@
 const Banner = ({src, headingText, subText}) => {
-	console.log(src);
 	return (
 		<img src={src} className="object-cover w-screen" style={{"height":"522px"}}  />
-		
 	)
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../components/Banner";
 import BannerImage from "/images/CleaningServiceGroup4Back.jpeg";
+import AboutImage from "/images/CleaningServiceGroup.jpeg"
+import { Link } from "react-router-dom";
 
 const Home = () => {  
     return (
-        <div>
+        <div className="text-inter">
             <div className="relative">
                 <Banner src={BannerImage}/>
                 <div className="absolute inset-0 bg-black bg-opacity-50 text-white" >
@@ -11,13 +13,28 @@ const Home = () => {
                         <div className="flex flex-col space-y-4">
                             <p className="text-5xl text-accent font-extrabold">Expert Cleaning Solutions.</p>
                             <p className="text-5xl text-accent font-extrabold">Reliable, Trusted and Affordable.</p>
-                            
                             <p className="text-2xl">Highly Regarded Cleaning Services in Manila</p>
                         </div>
                         <button className="inline-flex bg-white text-accent text-2xl py-3 px-6 rounded font-medium duration-100 hover:scale-1">Learn More</button>
                     </div>
                 </div>
-            </div>  
+            </div>
+            <main className="grid place-items-center max-w-screen-xl mx-auto">
+                <div className = "grid grid-cols-2 place-items-center my-14 gap-12 p-3">
+                    <img src={AboutImage}/>
+                    <div className="flex flex-col h-full justify-between">
+                        <div className = "flex flex-col gap-4">
+                            <p className="text-2xl text-primary font-bold uppercase">Sweeping Masters Co.</p>
+                            <p className="text-5xl text-primary font-semibold">We care about our work.</p>
+                            <p className="text-xl">We at Sweeping Masters care about the well being of our clients and their homes, therefore we carefully tend to the requests and do even more. We are a company based in Marikina and offers services within Manila. We are dedicated in improving our crafts and into listening and appropriately responding the needs of our clients. </p>
+                        </div>
+                        <Link to = "/about" className="text-right text-accent text-2xl">About the company</Link>
+                    </div>
+                </div> 
+                <div>
+                    Services
+                </div> 
+            </main>  
         </div>
     )
 }
