eaca32f338d60c5cf4905426f85a37199103e1b5
Arc-Data <arclight04@gmail.com>
2023-07-06 10:21:16 +0800

    fixed image import error and added description text

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,11 @@
-const Card = ({title, src}) => {
+const Card = ({title, src, description}) => {
 	console.log(title, src);
 	return (
 		<div className="bg-white rounded-lg shadow flex-col" style={{"height": "436px"}} >
 			<img src={src} alt="Nice" className="object-cover h-3/5 w-full rounded-t-lg"/>
 			<div className="flex-1 p-3.5 space-y-10">
 				<p className="text-primary text-2xl font-bold">{title}</p>
-				<p>Insert Description</p>
+				<p className="line-clamp-3">{description}</p>
 			</div>
 		</div>
 	)	
