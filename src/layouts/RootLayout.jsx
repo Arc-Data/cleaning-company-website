85c76815b09e6b0cefd80e679bcf5a99004aa9cb
Arc-Data <arclight04@gmail.com>
2023-07-07 19:11:21 +0800

    mobile nav adjustments

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -3,25 +3,25 @@ import { Link, Outlet } from "react-router-dom";
 const RootLayout = () => {
     return (
         <>
-        <header className ="shadow-md sticky top-0 p-5 font-inter z-10 bg-white">
-            <div className="container mx-auto flex items-center content-end">
-                <img src="images/Logo-Transparent.png" width="130px" height="130px"/>
-                <nav className="ml-auto flex flex-col space-y-10">
-                    <div className="info flex text-xs gap-2.5">
+        <header className ="shadow-md sticky top-0 p-6 font-inter z-10 bg-white">
+            <div className="container mx-auto flex items-center content-end md:justify-between">
+                <img src="images/Logo-Transparent.png" className="hidden md:block w-32 h-auto"/>
+                <nav className="flex flex-col md:space-y-10">
+                    <div className="hidden md:flex info text-xs justify-end space-x-5">
                        <p>
-                           Email: sweepingmasters@gmail.com
+                       <i class="fa-solid fa-envelope"></i> sweepingmasters@gmail.com
                        </p>
                        <p>
-                           Contact No.: 63+ 920-275-0407
+                       <i class="fa-solid fa-phone"></i> 63+ 920-275-0407
                        </p>
                     </div>
-                    <div className="navlinks flex gap-12 items-center">
-                        <Link to = "/">Home</Link>
-                        <Link to = "/about">About</Link>
-                        <Link to = "/services">Services</Link>
-                        <Link to = "/contact">Contact</Link>
+                    <div className="navlinks flex gap-12 items-center overflow-auto uppercase font-semibold text-primary">
+                        <Link to = "/" className=" hover:bg-gray-300 p-4 rounded-2xl">Home</Link>
+                        <Link to = "/about" className=" hover:bg-gray-300 p-4 rounded-2xl">About</Link>
+                        <Link to = "/services" className=" hover:bg-gray-300 p-4 rounded-2xl">Services</Link>
+                        <Link to = "/contact" className=" hover:bg-gray-300 p-4 rounded-2xl">Contact</Link>
                         <Link to = "/book">
-                            <button className = "bg-primary text-white py-1 px-3.5 rounded">Book Now!</button>
+                            <button className = "bg-primary text-white p-4 rounded uppercase hover:opacity-80">Book Now!</button>
                         </Link>
                     </div>
                 </nav>
