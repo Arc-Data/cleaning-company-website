e6695adb384b5fe9e6bf61f487974ed1f6be238a
Arc-Data <arclight04@gmail.com>
2023-07-07 19:23:07 +0800

    navbar adjustments - part 2

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -3,8 +3,8 @@ import { Link, Outlet } from "react-router-dom";
 const RootLayout = () => {
     return (
         <>
-        <header className ="shadow-md sticky top-0 p-6 font-inter z-10 bg-white">
-            <div className="container mx-auto flex items-center content-end md:justify-between">
+        <header className ="container overflow-x-auto whitespace-nowrap md:overflow-visible md:whitespace-normal shadow-md sticky top-0 p-6 font-inter z-10 bg-white">
+            <div className="mx-auto flex items-center content-end md:justify-between">
                 <img src="images/Logo-Transparent.png" className="hidden md:block w-32 h-auto"/>
                 <nav className="flex flex-col md:space-y-10">
                     <div className="hidden md:flex info text-xs justify-end space-x-5">
@@ -15,7 +15,7 @@ const RootLayout = () => {
                        <i class="fa-solid fa-phone"></i> 63+ 920-275-0407
                        </p>
                     </div>
-                    <div className="navlinks flex gap-12 items-center overflow-auto uppercase font-semibold text-primary">
+                    <div className="flex gap-12 items-center uppercase font-semibold text-primary">
                         <Link to = "/" className=" hover:bg-gray-300 p-4 rounded-2xl">Home</Link>
                         <Link to = "/about" className=" hover:bg-gray-300 p-4 rounded-2xl">About</Link>
                         <Link to = "/services" className=" hover:bg-gray-300 p-4 rounded-2xl">Services</Link>
