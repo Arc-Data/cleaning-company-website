0fb288968ed6ec7e7c4e4f15b7ff54ba230d3e58
Arc-Data <arclight04@gmail.com>
2023-07-08 12:26:00 +0800

    home banner small screen adjustments

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -44,11 +44,11 @@ const Home = () => {
             <div className="relative">
                 <Banner src={BannerImage}/>
                 <div className="absolute inset-0 bg-black bg-opacity-50 text-white" >
-                    <div className="flex flex-col w-3/5 h-full space-y-10 justify-center ml-20 items-start">
+                    <div className="container flex flex-col h-full w-full p-4 md:mx-auto space-y-10 justify-center items-start">
                         <div className="flex flex-col space-y-4">
-                            <p className="text-5xl text-accent font-extrabold">Expert Cleaning Solutions.</p>
-                            <p className="text-5xl text-accent font-extrabold">Reliable, Trusted and Affordable.</p>
-                            <p className="text-2xl">Highly Regarded Cleaning Services in Manila</p>
+                            <p className="text-4xl text-accent font-extrabold">Expert Cleaning Solutions.</p>
+                            <p className="text-4xl text-accent font-extrabold">Reliable, Trusted and Affordable.</p>
+                            <p className="text-xl">Highly Regarded Cleaning Services in Manila</p>
                         </div>
                         <button className="inline-flex bg-white text-accent text-2xl py-3 px-6 rounded font-medium duration-100 hover:scale-1">Learn More</button>
                     </div>
