f0d219f0d63cb9b990df617e8c5231f090f8e738
Arc-Data <arclight04@gmail.com>
2023-07-08 18:10:01 +0800

    adds services page

---
+++ src/data/cards.js
@@ -0,0 +1,35 @@
+const Cards = [
+    {
+        "title": "Post Construction Cleaning Services",
+        "src": "/images/Staircase.jpeg",
+        "description": "Don't know where to start after getting your house renovated or constructed? Our post-construction cleaning services are perfect for you! We will remove all leftover dust, debris, and dirt, transitioning your home and making it clean as new!"
+    },
+    {
+        "title": "Residential Cleaning Services",
+        "src": "/images/FloorMop.jpeg",
+        "description": "Have you ever dreamt of coming home to your apartment and not ever thinking of your cleaning responsibilities? Well, this service is right up your alley! Our professionals will clean and handle your apartment, making it comfortable, dust and dirt-free!"
+    },
+    {
+        "title": "Residential Deep Cleaning Services",
+        "src": "/images/SofaVacuum.jpeg",
+        "description": "Sometimes, the nooks and crannies in our home are difficult to reach and clean, making dust gather in one place. Let our professionals handle all your hassles with our deep cleaning services! We can guarantee your satisfaction and can accommodate your personal requests."
+    },
+    {
+        "title": "Commercial Deep Cleaning Services",
+        "src": "/images/RoomMirror.jpeg",
+        "description": "General cleaning is usually not enough to remove the built-up dirt and dust. Our commercial deep-cleaning services will ease your cleaning responsibilities. With our professionals, we can easily remove the built-up dirt, dust, and unnecessary hazards in your establishments."
+    },
+    {
+        "title": "House/Office Keeping",
+        "src": "/images/RoomCleaning.jpeg",
+        "description": "Work will always get busy as it gets. Do you find yourself having no time to clean? Well, we have professionals who can do it for you! We offer one-time or regular office or housekeeping cleaning services that fit your needs! Rest assured that our services will make your office or home a comfortable and cozy atmosphere."
+    },
+    {
+        "title": "Commerical Cleaning Services",
+        "src": "/images/Window.jpeg",
+        "description": "Daily, customers and employees come and go in buildings and establishments. It always is messy right after a business week. Let our company's professionals handle all your cleaning needs! We offer a variety of services depending on your type of establishment or business. Book now!"
+    },
+];
+
+
+export default Cards;
--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,11 +1,22 @@
 import BannerOverlay from "../components/BannerOverlay";
 import Footer from "../components/Footer";
 import BannerImage from "/images/CleaningServiceGroup4Back.jpeg";
+import Cards from "../data/cards";
+import Card from "../components/Card";
+
+console.log(Cards)
 
 const Service = () => {
     return (
         <div>  
             <BannerOverlay src={BannerImage} title={"Services"} subtitle={"We got what you need!"}/>
+            <section className="p-6 md:p-32">
+                <div className="container mx-auto grid md:grid-cols-3 gap-10">
+                    {Cards.map((card, idx) => {
+                        return <Card key={idx} title={card.title} src={card.src} description={card.description}/> 
+                    })}
+                </div>
+            </section>
             <Footer />
         </div>
     )
